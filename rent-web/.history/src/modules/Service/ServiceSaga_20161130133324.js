e24d79d32542b4fc3e6fe9a9e9bf4bf70740ec3f
Dmitry Saltykov <d.saltykov@yandex.ru>
2016-12-02 11:16:35 +0800

    remove .history

--- rent-web/.history/src/modules/Service/ServiceSaga_20161130133324.js
+++
@@ -1,9 +0,0 @@
-// Import Saga
-import { rootServiceTypeSaga } from './sagas/ServiceTypeSaga';
-import { rootServiceSaga } from './sagas/ServiceSaga';
-
-// Export Saga
-export default [
-  ...rootServiceTypeSaga,
-  ...rootServiceSaga,
-];
