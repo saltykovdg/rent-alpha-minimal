7143ca3c39af6c2704335e1dc34d4339baa1038c
Dmitry Saltykov <d.saltykov@yandex.ru>
2016-12-02 11:26:41 +0800

    Initial commit 3

--- rent-web_spring/src/main/sources/views/main.controller.js
+++
@@ -1,9 +0,0 @@
-export default ($scope, $rootScope, $timeout, properties) => {
-    var vm = $scope;
-    $rootScope.properties = properties;
-
-    $timeout(() => {
-        //init header
-        $('[name=header] .dropdown').dropdown();
-    }, 0, false);
-}
