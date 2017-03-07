b355b0f5b97e9225f089d89b4f8a83d5fff190f8
Dmitry Saltykov <d.saltykov@yandex.ru>
2017-03-08 02:39:58 +0800

    fix

--- rent-web/webpack.config.prod.js
+++ rent-web/webpack.config.prod.js
@@ -60,7 +60,7 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
         'RENT_API_URL': JSON.stringify('/rent-api'),
-        'RENT_API_CONTENT_URL': JSON.stringify('/content/')
+        'RENT_API_CONTENT_URL': JSON.stringify('content')
       }
     }),
     new webpack.optimize.CommonsChunkPlugin({
