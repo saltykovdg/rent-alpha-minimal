bdcc89b5c0124a40dbf4b32191608a7b8ed7ecd4
Dmitry Saltykov <d.saltykov@yandex.ru>
2017-07-08 16:00:27 +0800

    format code

--- rent-web/webpack.config.prod.local.js
+++ rent-web/webpack.config.prod.local.js
@@ -2,14 +2,14 @@ const webpack = require('webpack');
 const baseConfig = require('./webpack.config.prod.base');
 
 baseConfig.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-        RENT_API_URL: JSON.stringify('/rent-api'),
-        RENT_API_CONTENT_URL: JSON.stringify('/content'),
-        RENT_API_MAX_FILE_SIZE: JSON.stringify('15728640'),
-      },
-    })
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+      RENT_API_URL: JSON.stringify('/rent-api'),
+      RENT_API_CONTENT_URL: JSON.stringify('/content'),
+      RENT_API_MAX_FILE_SIZE: JSON.stringify('15728640'),
+    },
+  })
 );
 
 module.exports = baseConfig;
