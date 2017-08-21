1fde892089a8ce76186d3e5071cbc56832a7cdc4
Dmitry Saltykov <d.saltykov@yandex.ru>
2017-08-21 22:08:46 +0800

    bug fix

--- rent-web/src/modules/App/pages/WelcomePage.js
+++ rent-web/src/modules/App/pages/WelcomePage.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-function WelcomePage() {
-  return (
-    <div>
-      <h1>
-        <FormattedMessage id="welcomePageTitle" />
-      </h1>
-      <p>
-        <FormattedMessage id="welcomePageText" />
-      </p>
-    </div>
-  );
+import { ExtendedComponentPage } from './../../../components/ExtendedComponentPage';
+
+class WelcomePage extends ExtendedComponentPage {
+  render() {
+    return (
+      <div>
+        <h1>
+          <FormattedMessage id="welcomePageTitle" />
+        </h1>
+        <p>
+          <FormattedMessage id="welcomePageText" />
+        </p>
+      </div>
+    );
+  }
 }
 
 export default WelcomePage;
